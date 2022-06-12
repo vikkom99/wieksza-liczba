47cab70aa33492d977ec523eaac4b1c5333a9221
vikkom99 <wikkomarik@gmail.com>
2022-06-12 22:37:05 +0100

    Poprawione

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let num1 = 2;
 let num2 = 5;
 
-function bigNum(num1, num2){
-   let result =  num1 >= num2;
+function bigNum(a, b){
+   let result = Math.max(a,b)
    return result
 }
 console.log( bigNum(num1, num2) );
